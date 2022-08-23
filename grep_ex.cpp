fe2575d2edf4379d31f7a00c6774596cd7f63fab
followthwhiterabbit <kaankaraalioglu@yahoo.com>
2022-08-23 18:24:03 +0200

    struggling with current_path()

--- grep_ex.cpp
+++ grep_ex.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
-#include <stdio.h>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
 #include <string>
 #include <filesystem>
-#include <fstream>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/stat.h>
 #include <sstream>
 #include <dirent.h>
+#include <unordered_map>
+
 
 
 using namespace std; 
+using std::filesystem::current_path;
 namespace fs = std::filesystem;
 
 // #define DIR_PATH = "/home/zornic/projects/grep_cpp"
@@ -15,13 +23,21 @@ namespace fs = std::filesystem;
 
 
  
-void text_file_set(std::string path, std::string search)
+void  text_file_set( fs::path path_to_search , std::string search_str)
 { 
+    
+    string search = search_str; 
+
+    fs::path p = path_to_search; 
+
+    int searched_files = 0, files_w_pattern = 0, patterns_number = 0; 
+
+
      std::ofstream txt_file; 
       txt_file.open("grep_ex.txt"); 
   
   
-    for (const auto  & entry : fs::recursive_directory_iterator(path))
+    for (const auto  & entry : fs::recursive_directory_iterator(p))
         {   
 
             int line_no = 0; 
@@ -35,17 +51,26 @@ void text_file_set(std::string path, std::string search)
                 auto pos = line.find(search); 
                 if(pos != string::npos)
                     {
+                        files_w_pattern++; 
                         txt_file << entry.path(); 
                         txt_file << ":" << line_no << ":" << line << endl; 
                     }
+                if(line_no > 100)
+                    return; 
 
              }
            //  log_file << entry.path() << std::endl;
 
+            searched_files++; 
+       
         }
+          txt_file << searched_files << " " << files_w_pattern;
         txt_file.close(); 
 
+
 }
+
+
 
 
 
@@ -53,153 +78,64 @@ void text_file_set(std::string path, std::string search)
 
 int main(int argc, char* argv[])
 {
-    /*
+    
+    
 
-    if(argc % 2 == 1) // if ./grepex executed once, the following instructions will be prompted 
-    {   
-        cout << "Grepx help: Please insert with the following format\n";
-        cout << "Usage: " << argv[0] << " [search_string] [-d][directory] [-l][log file] " 
-                                        "[r][result file] [-t][threads]\n"; 
-        return 1; 
-    }
+
+    // if(argc % 2 == 1) // if ./grepex executed once, the following instructions will be prompted 
+    // {   
+    //     cout << "Grepx help: Please insert with the following format\n";
+    //     cout << "Usage: " << argv[0] << " [search_string] [-d][directory] [-l][log file] " 
+    //                                     " [r][result file] [-t][threads]\n"; 
+    //     return 1; 
+    // }
+    
+
+     
+
+    // if(argc == 2)
+    // {
+    //    text_file_set(fs::current_path(), argv[1]); 
+    // } 
+    
+
+
+    //   if (argc == 4 && argv[2] == "-d")
+    //  {
+    //     string directory = argv[3]; 
+
+     
+  
+    //       text_file_set(directory, argv[1]); 
+    //  }
+
+
+    // if(argc == 6 && argv[2] == "-d" && argv[4] == "-l")
+    // {
+  
+
+    // }
+    
+    // if (argc == 8 && argv[2] == "-d" && argv[4] == "-l" && argv[6] == "-r" )
+    //     { 
  
-    string search;         
-    if(argc == 2)
-    {
-         search = argv[1];
-        ofstream log_file; 
-        ofstream result_file; 
 
-        string log_filename = argv[0];
-        log_filename += ".log"; 
-
-        string result_filename = argv[0]; 
-        result_filename += ".txt"; 
-
-        log_file.open(log_filename);
-        result_file.open(result_filename); 
-    }
-
-    if(argc == 4 && argv[2] == "-d")
-    {
-         search = argv[1]; 
-        string directory = argv[3]; 
-
-        ofstream log_file; 
-        ofstream result_file; 
-
-        string log_filename = argv[0];
-        log_filename += ".log"; 
-
-        string result_filename = argv[0]; 
-        result_filename += ".txt"; 
-
-        log_file.open(log_filename);
-        result_file.open(result_filename); 
-
-    }
-
-    if(argc == 6 && argv[2] == "-d" && argv[4] == "-l")
-        {
-         search = argv[1]; 
-        string directory = argv[3]; 
-
-        ofstream log_file; 
-        ofstream result_file; 
-
-        string log_filename = argv[5];
-        log_filename += ".log"; 
-
-        string result_filename = argv[0]; 
-        result_filename += ".txt"; 
-
-         log_file.open(log_filename);
-        result_file.open(result_filename); 
-
-        }
-    
-    if(argc == 8 && argv[2] == "-d" && argv[4] == "-l" && argv[6] == "-r" )
-        {
-         search = argv[1]; 
-        string directory = argv[3]; 
-
-        ofstream log_file; 
-        ofstream result_file; 
-
-        string log_filename = argv[5];
-        log_filename += ".log"; 
-
-        string result_filename = argv[7]; 
-        result_filename += ".txt"; 
-
-         log_file.open(log_filename);
-        result_file.open(result_filename); 
-        }
-    
-
-*/ 
-
-
-
-
- /*
-
-    
-    ifstream infile(argv[2]); 
-    while(!infile)
-    {
-        cout << "Cannot open " << argv[2] << '\n';  
-        return 1; 
-    }
-
-        ofstream log_file; 
-        log_file.open("grep_ex.log"); 
-
-
-    int lineno = 0;
-    string line; 
-    while(getline(infile, line))
-    {
-        ++lineno; 
-        auto pos = line.find(argv[1]); 
-        if (pos != string::npos)
-            {
-                log_file << lineno << ":" << line <<'\n'; 
-            }
-       
-    }
-    
-         log_file.close();
-            
-
-   */   
+    //     }
    
+ // tfs::path curent_p = fs::current_path(); 
+ //text_file_set("/home/zornic/Desktop/test", "seyit"); 
+//text_file_set("/home/zornic/projects/grep_cpp", "karaalioglu"); 
 
 
-/*
-   
-    std::ofstream log_file; 
-    log_file.open("grep_ex.log"); 
+//text_file_set("/home/zornic/projects/grep_cpp", "seyit"); 
 
 
-
-   std::string path = "/home/zornic/projects/grep_cpp";
-    for (const auto & entry : fs::recursive_directory_iterator(path))
-        {    
-            log_file << entry.path() << std::endl;
-
-        }
-
-        log_file.close(); 
-
-*/ 
-
-
-
- text_file_set("/home/zornic/Desktop/test", "seyit"); 
-
+//char *cwd = get_current_dir_name();
 
 
 
 
 }
+
+// "/home/zornic/Desktop/test" // test directory 
+ // "/home/zornic/projects/grep_cpp" // grep_directory 
