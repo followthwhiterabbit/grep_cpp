a675cd509634c18a368e561ee808b6f91376467b
followthwhiterabbit <kaankaraalioglu@yahoo.com>
2022-08-24 09:19:45 +0200

    working on

--- grep_ex.cpp
+++ grep_ex.cpp
@@ -10,7 +10,7 @@
 #include <sys/stat.h>
 #include <sstream>
 #include <dirent.h>
-#include <unordered_map>
+#include <vector>
 
 
 
@@ -23,9 +23,9 @@ namespace fs = std::filesystem;
 
 
  
-void  text_file_set( fs::path path_to_search , std::string search_str)
+std::vector<int>  grep_func( fs::path path_to_search , std::string search_str)
 { 
-    
+    std::vector<int> res; 
     string search = search_str; 
 
     fs::path p = path_to_search; 
@@ -56,36 +56,44 @@ void  text_file_set( fs::path path_to_search , std::string search_str)
                         txt_file << ":" << line_no << ":" << line << endl; 
                     }
                 
+                /*
                 if(line_no > 200)
-                    return; 
-                
+                    return res; 
+                */
              }
            //  log_file << entry.path() << std::endl;
 
             searched_files++; 
        
         }
-          txt_file << searched_files << " " << files_w_pattern;
         txt_file.close(); 
 
+    res.push_back(searched_files); 
+    res.push_back(files_w_pattern); 
+    res.push_back(patterns_number); 
+
+return res; 
 
 }
 
 
 
 
-void log_file_set()
+void display_help()
 {
+        cout << "Grepx help: Please insert with the following format\n";
+        cout << "Usage: " << "./grepex " << " [search_string] [-d][directory] [-l][log file] " 
+                                        " [r][result file] [-t][threads]\n"; 
+}
 
 
 
-
-
-
-
-
-
-
+void display_results(int s, int f, int p)
+{
+    std::cout << "Searched files: " << s << std::endl; 
+    std::cout << "Files with pattern: " << f << std::endl; 
+    std::cout << "Patterns_number: " << p << std::endl; 
+    
 }
 
 
@@ -96,17 +104,35 @@ void log_file_set()
 
 int main(int argc, char* argv[])
 {
+    int searched_files; 
+    int files_w_pattern; 
+    int patterns_number; 
+
+    std::vector<int> results; 
+
+    // setting up the flags 
+    std::string flag1;
+    std::string flag2;
+    std::string flag3;
+    std::string flag4;
+    std::string d_flag = "-d"; 
+    std::string l_flag = "-l";
+    std::string r_flag = "-r";
+    std::string t_flag = "-t"; 
+
+
+    
+
     string given_exec = argv[0]; 
     string file_exec = "./grepex"; 
 
+     
     
     
 
     if(argc % 2 == 1) // if ./grepex executed once, the following instructions will be prompted 
     {   
-        cout << "Grepx help: Please insert with the following format\n";
-        cout << "Usage: " << argv[0] << " [search_string] [-d][directory] [-l][log file] " 
-                                        " [r][result file] [-t][threads]\n"; 
+        display_help(); 
         return 1; 
     }
     
@@ -117,29 +143,136 @@ int main(int argc, char* argv[])
     if (argc == 2)
     {   search = argv[1]; 
         fs::path cwd = fs::current_path();   
-        text_file_set(cwd, search); 
+        results = grep_func(cwd, search); 
+        searched_files = results[0]; 
+        files_w_pattern = results[1]; 
+        patterns_number = results[2]; 
+        display_results(searched_files, files_w_pattern, patterns_number); 
     }
-
     else if (argc == 4 && given_exec == file_exec)
     {
-        string directory = argv[3]; 
-        text_file_set(directory, argv[1]); 
+        flag1 = argv[2]; 
+
+        if(flag1 == d_flag)
+            string directory = argv[3]; 
+        else if(flag1 == l_flag)
+            {
+
+            }
+        else if(flag1 == r_rflag)
+            {
+
+            }
+        else if(flag1 == t_flag)
+            {
+
+            }
+        else
+        {
+            display_help();
+            return 1; 
+
+        }
+
+
+
+        results = grep_func(directory, argv[1]); 
+        searched_files = results[0]; 
+        files_w_pattern = results[1]; 
+        patterns_number = results[2]; 
+        display_results(searched_files, files_w_pattern, patterns_number); 
     }
   
     
    
-   //text_file_set("/home/zornic/projects/grep_cpp","karaalioglu"); 
+   //grep_func("/home/zornic/projects/grep_cpp","karaalioglu"); 
+
+     if (argc == 6)
+     {
+        flag1 = argv[2];
+        flag2 = argv[4]; 
+        if(flag1 == d_flag && flag2 == l_flag)
+        {
+
+        }
+        else if(flag1 == l_flag && flag2 == r_flag)
+        {
+
+
+        }
+        else if(flag1 == r_flag && flag2 == t_flag)
+        {
+
+        }
+        
+    }
+    if(argc == 8)
+    {   
+        flag1 = argv[2];
+        flag2 = argv[4];
+        flag3 = argv[6];
+        
+        if(flag1 == d_flag && flag2 == l_flag && flag3 == r_flag)
+        {
+
+        }
+        else if(flag1 == l_flag && flag2 == r_flag && flag3 == t_flag)
+        {
+            
+
+        }
+        else if(flag1 == d_flag && flag2 == r_flag && flag3 == t_flag)     
+        {
+
+
+        }
+        else
+        {
+            display_help();
+            return 1; 
+        }
+
+    }
+    
+    if (argc == 10)
+    {
+        flag1 = argv[2];
+        flag2 = argv[4];
+        flag3 = argv[6];
+        flag4 = argv[8];
+        
+        if(flag1 == d_flag && flag2 == l_flag && flag3 == r_flag && t_flag)
+        {
+
+
+
+
+        }
+        else 
+        {
+            display_help();
+            return 1; 
+        }
 
 
 
 
 
 
-    // if (argc == 6 && argv[2] == "-d" && argv[4] == "-l")
-    // {
+
+
+
+
+
+
+
+
+    }
+
+
   
 
-    // }
+     
     
     // if (argc == 8 && argv[2] == "-d" && argv[4] == "-l" && argv[6] == "-r" )
     //     { 
@@ -148,16 +281,16 @@ int main(int argc, char* argv[])
     //     }
    
  // tfs::path curent_p = fs::current_path(); 
- //text_file_set("/home/zornic/Desktop/test", "seyit"); 
-//text_file_set("/home/zornic/projects/grep_cpp", "karaalioglu"); 
+ //grep_func("/home/zornic/Desktop/test", "seyit"); 
+//grep_func("/home/zornic/projects/grep_cpp", "karaalioglu"); 
 
 
-//text_file_set("/home/zornic/projects/grep_cpp", "seyit"); 
+//grep_func("/home/zornic/projects/grep_cpp", "seyit"); 
 
 
 
 
-//text_file_set(argv[3], argv[1]); 
+//grep_func(argv[3], argv[1]); 
 
 
 
