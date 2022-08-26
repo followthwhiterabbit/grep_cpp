699926ee1fdccd6e287257214fcb151d64786047
followthwhiterabbit <kaankaraalioglu@yahoo.com>
2022-08-26 22:47:56 +0200

    thread first

--- grep_ex.cpp
+++ grep_ex.cpp
@@ -12,10 +12,10 @@
 #include <dirent.h>
 #include <vector>
 #include <chrono>       
-#include <future>
 #include <map>
 #include <iterator>
 #include <algorithm>
+#include <thread> 
 
 
 
@@ -32,10 +32,11 @@ bool compare_dir(std::pair<std::string, int>&a,
     return a.second > b.second; 
 }
 
-std::map<string, int> sort_func(std::map<std::string, int>& M)
+std::vector<std::pair<std::string, int>> sort_func(std::map<std::string, int>& M)
 {
     
     std::vector<pair<string, int>> temp;
+
 
     for(auto &it : M)
         temp.push_back(it); 
@@ -44,21 +45,18 @@ std::map<string, int> sort_func(std::map<std::string, int>& M)
     sort(temp.begin(), temp.end(), compare_dir); 
 
 
-    std::map<string, int> result; 
-
-    for(auto &el : temp)
-        result[el.first] = el.second; 
-
-    
-    return result; 
+    return temp; 
 
 }
  
-std::vector<int>  grep_func(fs::path path_to_search, std::string search_str, std::string logfname, std::string txtfname)
+void 
+grep_func(fs::path path_to_search, std::string search_str, std::string logfname, std::string txtfname, std::vector<int> &results)
 { 
-    map<int, int>::iterator itr;
+    std::vector<std::pair<std::string, int>> sorted; 
+    std::vector<std::pair<std::string, int>>::iterator sortitr; 
+    std::map<int, int>::iterator itr;
     std::vector<int> res; 
-    string search = search_str; 
+    std::string search = search_str; 
 
     fs::path p = path_to_search; 
 
@@ -145,28 +143,38 @@ std::vector<int>  grep_func(fs::path path_to_search, std::string search_str, std
            //  log_file << entry.path() << std::endl;
 
             searched_files++; 
-     
+            sorted = sort_func(m);
+            //txt_file << entry.path(); 
+
         }
 
+        //sorted = sort_func(m); 
+
+        // txt_file << el.first << std::endl; 
+
+        
+          for(auto sortitr = sorted.begin(); sortitr != sorted.end(); sortitr++)
+            {
+                std::cout << sortitr->first << " " << sortitr->second << endl; 
+                txt_file << sortitr->first << std::endl;             
+
+            }
+         
 
         txt_file.close(); 
    
+        
          for(auto itr = m.begin(); itr != m.end(); ++itr)
             {
-                //std::cout << itr->first << " " << itr->second << std::endl; 
                 patterns_number += itr->second; 
             }
-         
+          
+
         files_w_pattern = m.size();
 
-
-
-
-    res.push_back(searched_files); 
-    res.push_back(files_w_pattern); 
-    res.push_back(patterns_number); 
-
-return res; 
+    results.push_back(searched_files); 
+    results.push_back(files_w_pattern); 
+    results.push_back(patterns_number); 
 
 }
 
@@ -208,11 +216,12 @@ int main(int argc, char* argv[])
     int searched_files; 
     int files_w_pattern; 
     int patterns_number; 
-    int num_of_threads = 4; // default value for the number of threads 
+    size_t num_of_threads = 4; // default value for the number of threads 
 
 
 
     std::vector<int> results; 
+    std::vector<std::thread> threads; 
 
     // setting up the flags 
     std::string flag1;
@@ -266,11 +275,7 @@ int main(int argc, char* argv[])
 
     if (argc == 2)
     {    
-        results = grep_func(cwd.string(), search, def_log_name, def_txt_name); 
-        searched_files = results[0]; 
-        files_w_pattern = results[1]; 
-        patterns_number = results[2]; 
-        display_results(searched_files, files_w_pattern, patterns_number); 
+        grep_func(cwd.string(), search, def_log_name, def_txt_name, results); 
     }
     else if (argc == 4 && given_exec == file_exec)
     {   
@@ -282,35 +287,37 @@ int main(int argc, char* argv[])
         if(flag1 == d_flag)
         {
             directory_value = argv[3];
-            results = grep_func(directory_value, search, def_log_name, def_txt_name);
-            searched_files = results[0]; 
-            files_w_pattern = results[1]; 
-            patterns_number = results[2]; 
-            display_results(searched_files, files_w_pattern, patterns_number);
+            grep_func(directory_value, search, def_log_name, def_txt_name, results);
         }
         else if(flag1 == l_flag)
             {
                 log_file_name = argv[3];
-                results = grep_func(cwd.string(),search, log_file_name, def_txt_name); 
-                searched_files = results[0]; 
-                files_w_pattern = results[1]; 
-                patterns_number = results[2]; 
-                display_results(searched_files, files_w_pattern, patterns_number);
+                grep_func(cwd.string(),search, log_file_name, def_txt_name, results); 
+
 
             }
         else if(flag1 == r_flag)
             {
                 text_file_name = argv[3];
-                results = grep_func(cwd.string(),search, def_log_name, text_file_name); 
-                searched_files = results[0]; 
-                files_w_pattern = results[1]; 
-                patterns_number = results[2]; 
-                display_results(searched_files, files_w_pattern, patterns_number);
+                grep_func(cwd.string(),search, def_log_name, text_file_name, results); 
+            
 
 
             }
         else if(flag1 == t_flag) // I DON'T KNOW THIS YET 
             {
+  
+                num_of_threads = atoi(argv[3]); 
+                for( size_t i = 0; i < num_of_threads; i++) // cwd.string() is an r-value already,
+                    {                                       // but we need to send the others as std::ref 
+                        threads.emplace_back(grep_func, cwd.string(), std::ref(search), std::ref(def_log_name),std::ref(text_file_name), std::ref(results)); 
+                    }
+
+                for(auto & thread : threads)
+                    {
+                        thread.join(); 
+                    }
+    
 
 
 
@@ -342,27 +349,21 @@ int main(int argc, char* argv[])
         {
             directory_value = argv[3];
             log_file_name = argv[5];
-            results = grep_func(directory_value,search, log_file_name, def_txt_name);
-            searched_files = results[0]; 
-            files_w_pattern = results[1]; 
-            patterns_number = results[2]; 
-            display_results(searched_files, files_w_pattern, patterns_number);
+            grep_func(directory_value,search, log_file_name, def_txt_name, results);
+            
 
         }
         else if(flag1 == l_flag && flag2 == r_flag)
         {
             log_file_name = argv[3]; 
             text_file_name = argv[5]; 
-            results = grep_func(cwd.string(),search, log_file_name, text_file_name);
-            searched_files = results[0]; 
-            files_w_pattern = results[1]; 
-            patterns_number = results[2]; 
-            display_results(searched_files, files_w_pattern, patterns_number);
+            grep_func(cwd.string(),search, log_file_name, text_file_name, results);
 
         }
         else if(flag1 == r_flag && flag2 == t_flag) // t flag not known yet 
         {
             text_file_name = argv[3]; 
+
 
 
 
@@ -380,11 +381,7 @@ int main(int argc, char* argv[])
             directory_value = argv[3]; 
             log_file_name = argv[5];
             text_file_name = argv[7];
-            results = grep_func(directory_value, search, log_file_name, text_file_name);
-            searched_files = results[0]; 
-            files_w_pattern = results[1]; 
-            patterns_number = results[2]; 
-            display_results(searched_files, files_w_pattern, patterns_number);
+            grep_func(directory_value, search, log_file_name, text_file_name, results);
 
         }
         else if(flag1 == l_flag && flag2 == r_flag && flag3 == t_flag)
@@ -450,7 +447,9 @@ int main(int argc, char* argv[])
 
 
 //grep_func(argv[3], argv[1]); 
-
+std::cout << "Searched files: " << results.at(0) << std::endl; 
+std::cout << "Files with pattern: " << results.at(1) << std::endl; 
+std::cout << "Patterns_number: " << results.at(2) << std::endl; 
 std::cout << "Result file: " << text_file_name << ".txt" << std::endl;
 std::cout << "Log file: " <<  log_file_name << ".log" << std::endl; 
 auto stop = high_resolution_clock::now();
